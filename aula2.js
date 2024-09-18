a785796208e800815c5cfe30124ff4c4bf674e98
adriangabrielmanoel <159543227+adriangabrielmanoel@users.noreply.github.com>
2024-09-18 08:53:39 -0300

    ok

---
+++ aula2.js
@@ -0,0 +1,24 @@
+//declaracao variaveis
+let numero1 = 10; //primeiro numero
+let numero2 = 5;  //segundo numero
+
+//operacoes aritimeticas
+let soma= numero1 + numero2;
+let subtracao = numero1-numero2
+let multiplicacao = numero1 * numero2
+let divisao= numero1/ numero2
+
+//exibe os resultados dos consoles
+console.log("soma" + soma);
+console.log("subtracao"+ subtracao);
+console.log("multiplicacao"+multiplicacao);
+console.log("divisao"+divisao);
+
+//declaracao de variaveis
+let largura=8; //largura do retangulo
+let altura=5;//altura do triangulo
+
+//calculo da area
+let area=largura * altura;
+//exibe o resultado do console
+console.log("a area do triangulo e"+ area);
