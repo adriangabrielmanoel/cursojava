22e5196067ac9461b0af27006dd02f417961c796
adriangabrielmanoel <159543227+adriangabrielmanoel@users.noreply.github.com>
2024-09-25 08:23:22 -0300

    eu

--- aula2.js
+++ aula2.js
@@ -22,3 +22,28 @@ let altura=5;//altura do triangulo
 let area=largura * altura;
 //exibe o resultado do console
 console.log("a area do triangulo e"+ area);
+//declaracao de variaveis 
+let celsius = 25; // temperatura em celcius 
+// conversao para fahrenheit 
+let fahrenheit= (celsius * 9/5)+32;
+
+//exibe o resultado no console 
+console.log(celsius+ "c e equivalente a" + fahrenheit+ " f");
+
+//declaracao de variaveis 
+let raio = 7; //raio do circuito 
+const pi = 3.14159; // valor de pi 
+
+// calculo do perimetro
+let perimetro = 2 * pi * raio;
+
+//exibe o resultado no console 
+console.log("o perimetro no circuito e:" + perimetro);
+
+// declaraao de variaveis 
+let precoOriginal=100; // preco original do produto 
+let desconto = 15;  // desconto em porcetagem 
+
+ // calculo do preco final 
+ let precoFinal= precoOriginal- (precooriginal* desconto / 100);
+ console.log("o preco final com desconto e R$ " + precoFinal)
